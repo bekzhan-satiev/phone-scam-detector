8cb67d374c82913e4054312e2ff4771226c1c8c4
Bekzhan Satiev <beksdeveloper@gmail.com>
2025-03-22 15:57:22 +0600

    Add Login Screen

--- app/src/main/java/kg/digitalshield/ui/screen/LoginScreen.kt
+++ app/src/main/java/kg/digitalshield/ui/screen/LoginScreen.kt
@@ -1,22 +1,120 @@
 package kg.digitalshield.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kg.digitalshield.R
+import kg.digitalshield.ui.component.TopRoundedColumn
 import kg.digitalshield.ui.theme.AppTheme
 
 @Composable
-fun HomeScreen() {
-    Column {
-        Text("Hello world!")
+fun LoginScreen(modifier: Modifier = Modifier) {
+    var login by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.schield),
+                contentDescription = stringResource(id = R.string.shield_image_description),
+                modifier = Modifier
+                    .size(250.dp)
+            )
+
+            Text(
+                modifier = Modifier.padding(top = 16.dp),
+                text = stringResource(id = R.string.digital_shield),
+                style = TextStyle(
+                    fontSize = 36.sp,
+                )
+            )
+        }
+
+        TopRoundedColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = login,
+                onValueChange = { login = it },
+                label = { Text(text = stringResource(id = R.string.phone_number)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = stringResource(id = R.string.password)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = stringResource(id = R.string.login))
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = stringResource(id = R.string.do_not_have_account))
+                Text(
+                    text = stringResource(id = R.string.register),
+                    modifier = Modifier.clickable { println("clicked!") },
+                    style = TextStyle(color = MaterialTheme.colorScheme.onPrimaryContainer)
+                )
+            }
+        }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable()
-fun HomeScreenPreview() {
+fun LoginScreenPreview() {
     AppTheme {
-        HomeScreen()
+        LoginScreen(modifier = Modifier.fillMaxSize())
     }
 }
